d282aba0528568bde351f0ac9bacf29a6080cace
Mike Erickson <codedungeon@gmail.com>
2013-12-22 21:51:44 -0800

    configured workbench file for artisan settings

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Mike Erickson',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'codedungeon@gmail.com',
 
 );
